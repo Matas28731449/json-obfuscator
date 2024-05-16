1a360a40d276d81d8d3174c26dfcc81e1f8b8de8
Matas28731449 <mataslekavi@gmail.com>
2024-05-16 23:28:56 +0300

    String replacement mapping in different formats

--- lib.hpp
+++ lib.hpp
@@ -3,54 +3,98 @@
 
 #include "utils.hpp"
 
-string cleanup(string str);
-string string_to_unicode(string str);
-void obfuscate(json &data);
-
 string cleanup(string str) {
-    string result;
-    result.reserve(str.length());
+    string cleanedStr;
+    cleanedStr.reserve(str.length());
     for (size_t i = 0; i < str.length(); i ++) {
         if (str[i] == '\\' && i + 1 < str.length() && str[i + 1] == '\\') {
-            result += '\\';
+            cleanedStr += '\\';
             i ++;
         } else {
-            result += str[i];
+            cleanedStr += str[i];
         }
     }
     
-    return result;
+    return cleanedStr;
 }
 
-string string_to_unicode(string str) {
+string stringToUnicode(Encoding encoding, string str) {
     stringstream ss;
-    for (char c : str) {
-        ss << "\\u" << setfill('0') << setw(4) << hex << static_cast<int>(c);
+    switch (encoding) {
+        case UTF32: {
+            wstring_convert<codecvt_utf8<char32_t>, char32_t> convert;
+            u32string u32str = convert.from_bytes(str);
+            for (char32_t c : u32str) {
+                ss << "U+" << setfill('0') << setw(8) << hex << static_cast<int>(c);
+            }
+            break;
+        }
+
+        case UTF16: {
+            wstring_convert<codecvt_utf8_utf16<char16_t>, char16_t> convert;
+            u16string u16str = convert.from_bytes(str);
+            for (char16_t c : u16str) {
+                ss << "\\u" << setfill('0') << setw(4) << hex << static_cast<int>(c);
+            }
+            break;
+        }
+
+        case UTF8: {
+            for (char c : str) {
+                ss << "\\x" << setfill('0') << setw(2) << hex << static_cast<int>(static_cast<unsigned char>(c));
+            }
+            break;
+        }
     }
+    
     return ss.str();
 }
 
-void obfuscate(json &data) {
+void dataToUnicode(Encoding type, unordered_map<string, string> &map, json &data) {
     if (data.is_object()) {
-        json obfuscated;
+        json encoded;
 
-        for (auto it = data.begin(); it != data.end(); it ++) {
+        for (auto it = data.begin(); it != data.end(); it++) {
             auto &key = it.key();
             auto &value = it.value();
 
-            json obfuscated_key = string_to_unicode(key);
-            obfuscate(value);
-            obfuscated.emplace(obfuscated_key.get<string>(), move(value));
+            dataToUnicode(type, map, value); // recursive approach
+
+            string defaultEncodedKey = stringToUnicode(UTF16, key), // default encoding
+                   formatEncodedKey = stringToUnicode(type, key); // encoded by a specified format type provided as an argument
+            map[key] = formatEncodedKey;
+            
+            encoded[defaultEncodedKey] = std::move(value);
         }
-        data = obfuscated;
+        data = encoded;
 
     } else if (data.is_array()) {
         for (auto &item : data) {
-            obfuscate(item);
+            dataToUnicode(type, map, item);
         }
 
     } else if (data.is_string()) {
-        data = string_to_unicode(data.get<string>());
+        string defaultEncodedValue = stringToUnicode(UTF16, data.get<string>()),
+               formatEncodedValue = stringToUnicode(type, data.get<string>());
+        map[data.get<string>()] = formatEncodedValue;
+        data = defaultEncodedValue;
+    }
+}
+
+void createReplacementMap(Encoding encoding, unordered_map<string, string> map) {
+    string str;
+
+    if (encoding == UTF8) {
+        str = "utf-8";
+    } else if (encoding == UTF16) {
+        str = "utf-16";
+    } else if (encoding == UTF32) {
+        str = "utf-32";
+    }
+
+    ofstream out("maps/replacement_map_" + str + ".txt");
+    for (auto pair : map) {
+        out << pair.first << " -> " << pair.second << "\n";
     }
 }
 
--- utils.hpp
+++ utils.hpp
@@ -2,17 +2,39 @@
 #define UTILS_HPP
 
 #include "nlohmann/json.hpp"
-#include <sstream>
+#include <iostream>
 #include <fstream>
+#include <sstream>
+#include <codecvt>
+#include <locale>
 
 using json = nlohmann::ordered_json;
+using std::unordered_map;
 using std::stringstream;
 using std::string;
+using std::cerr;
 using std::hex;
 using std::setw;
 using std::move;
 using std::setfill;
 using std::ifstream;
 using std::ofstream;
+using std::wstring_convert;
+using std::codecvt_utf8;
+using std::codecvt_utf16;
+using std::codecvt_utf8_utf16;
+using std::u16string;
+using std::u32string;
+
+enum Encoding {
+    UTF32,
+    UTF16,
+    UTF8
+};
+
+string cleanup(string str);
+string stringToUnicode(Encoding encoding, string str);
+void dataToUnicode(Encoding encoding, unordered_map<string, string> &map, json &data);
+void createReplacementMap(Encoding encoding, unordered_map<string, string> map);
 
 #endif
