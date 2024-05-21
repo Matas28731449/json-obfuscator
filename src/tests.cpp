ba435956b0cee8ee1f6964a516c6da441b35938c
Matas28731449 <mataslekavi@gmail.com>
2024-05-21 18:01:44 +0300

    JsonEncoder unit testing

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,5 +1,13 @@
-#include <gtest/gtest.h>
 #include "string_encoder.cpp"
+#include "json_encoder.cpp"
+#include <gtest/gtest.h>
+#include <fstream>
+#include <sstream>
+
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::stringstream;
 
 TEST(string_encoder, encodeToUtf32Check) {
     string value = "test_value_1";
@@ -26,9 +34,36 @@ TEST(string_encoder, encodeToUtf8Check) {
 }
 
 TEST(string_encoder, cleanUpJsonDumpCheck) {
-    string value = "\\\\x74\\\\x65\\\\x73\\\\x74\\\\x5f\\\\x76\\\\x61\\\\x6c\\\\x75\\\\x65\\\\x5f\\\\x34"; // test_value_4
-    string expected = "\\x74\\x65\\x73\\x74\\x5f\\x76\\x61\\x6c\\x75\\x65\\x5f\\x34"; // test_value_4
+    string value = R"(\\x74\\x65\\x73\\x74\\x5f\\x76\\x61\\x6c\\x75\\x65\\x5f\\x34)"; // test_value_4
+    string expected = "\\x74\\x65\\x73\\x74\\x5f\\x76\\x61\\x6c\\x75\\x65\\x5f\\x34";
     string actual = StringEncoder::cleanUpJsonDump(value);
+
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(json_encoder, encodeJsonCheck) {
+    string input = "input.json";
+    string output = "output.json";
+
+    ofstream out(input);
+    out << R"({
+        "test5": "value5",
+        "test6": "value6"
+    })";
+    out.close();
+
+    JsonEncoder obfuscator(input, output, Encoding::UTF16);
+    obfuscator.encodeJson();
+
+    ifstream in(output);
+    stringstream ss;
+    ss << in.rdbuf();
+
+    json actual = json::parse(ss.str());
+    json expected = json::parse(R"({
+        "\u0074\u0065\u0073\u0074\u0035": "\u0076\u0061\u006c\u0075\u0065\u0035",
+        "\u0074\u0065\u0073\u0074\u0036": "\u0076\u0061\u006c\u0075\u0065\u0036"
+    })");
 
     EXPECT_EQ(actual, expected);
 }
